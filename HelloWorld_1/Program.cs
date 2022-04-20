69e63b1d872b61b2a2bd7b63c20f790c6e3df0f6
seayki <101247473+seayki@users.noreply.github.com>
2022-04-20 10:10:31 +0200

    Update Program.cs

--- HelloWorld_1/Program.cs
+++ HelloWorld_1/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello CHRISTINA her er ISAK World!");
         }
     }
 }
